0c785988ee59f4bd9cb9e34da4e7e84be98bd949
harmony <sd.harmony@yahoo.fr>
2023-02-06 10:49:45 +0100

    fix fix fix

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -103,25 +103,25 @@ export type ListeUserForme = {
 };
 
 const App = () => {
-  const [trainers, gettrainers] = useState<ListeTrainerForme[] | null>();
+  const [users, getusers] = useState<ListeUserForme[] | null>();
   useEffect(() => {
-    const url = "http://localhost:3000/trainers";
+    const url = "http://localhost:3000/seealllog";
     axios.get(url).then((response) => {
-      gettrainers(response.data);
+      getusers(response.data);
     });
   }, []);
 
-  const [trainer, setTrainer] = useState<Trainer>({
-    name: "",
-    gender: "",
+  const [user, setUser] = useState<User>({
+    email: "",
+    pwd:"",
   });
 
   const setNewValue = (id_: string, newValue: string) =>
-    setTrainer((prevState) => ({ ...prevState, [id_]: newValue }));
+    setUser((prevState) => ({ ...prevState, [id_]: newValue }));
 
-  const createTrainer = async () => {
+  const createUser = async () => {
     try {
-      const response = await axios.post(urlTrainerPost, trainer);
+      const response = await axios.post(urlLoginPost, user);
       alert(`The reponse is: ${response.data.id}`);
     } catch (exception_) {
       alert(`There was an error `);
@@ -130,34 +130,34 @@ const App = () => {
   return (
     <div className="mt-10 text-3xl mx-auto max-w-6xl">
       <div className="div1">
-        <h1>Liste des dresseurs : </h1>
-        {trainers
-          ? trainers.map((train) => {
-              return <p> {train.name}</p>;
+        <h1>Liste des inscrits: </h1>
+        {users
+          ? users.map((train) => {
+              return <p> {train.email}</p>;
             })
           : null}
       </div>
       <div className="div2">
-        <h1>Ajouter un dresseur : </h1>
+        <h1>Inscription : </h1>
         <input
-          placeholder="name"
-          value={trainer.name}
+          placeholder="email"
+          value={user.email}
           onChange={(evt) => {
-            setNewValue("name", evt.target.value);
+            setNewValue("email", evt.target.value);
           }}
         />
         <br />
         <input
-          placeholder="gender"
-          value={trainer.gender}
+          placeholder="pwd"
+          value={user.pwd}
           onChange={(evt) => {
-            setNewValue("gender", evt.target.value);
+            setNewValue("pwd", evt.target.value);
           }}
         />
         <br />
         <button
           onClick={() => {
-            createTrainer();
+            createUser();
           }}
         >
           Enregister
