726f6f801bdd33c5f495368d6ccae03669d3530e
Harmony <sd.harmony@yahoo.fr>
2023-02-08 19:46:31 +0100

    envoie email apres inscription

--- apps/api/src/domain/create-user.usecase.ts
+++ apps/api/src/domain/create-user.usecase.ts
@@ -1,5 +1,6 @@
 import { Pokemon, User} from "./entities";
 import { IUserRepository } from "./interfaces";
+import nodemailer from "nodemailer";
 
 export class CreateUserUsecase {
     private userRepository: IUserRepository;
@@ -7,6 +8,7 @@ export class CreateUserUsecase {
     constructor(trainerRepository: IUserRepository) {
         this.userRepository = trainerRepository;
     }
+
 
     async execute(command: {name: string, pwd: string, email: string,
         inscriptionDate: Date, myTrainer: number , AllMyPokemon: Pokemon[]}): Promise<User> {
@@ -19,6 +21,29 @@ export class CreateUserUsecase {
             myTrainer: command.myTrainer,
             AllMyPokemon: [],
         });
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'pocamonteam@gmail.com',
+                pass: 'yofrtbppyhtmzmtd'
+            }
+        });
+        const mailOptions = {
+            from: 'pocamonteam@gmail.com',
+            to: command.email,
+            subject: 'Inscription pocamon',
+            text: 'MeRci PoUr vOtRe InScRiPtIoN 👍🏻'
+        };
+        transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+        console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+            // do something useful
+        }
+        });
+
+
         return newUser;
     }
 }
